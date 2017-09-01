53ae28b33ab77efd9c00f06141c2d56adfce69a7
ChanchaiKunnasut <c.kunnasut@togetherteam.co.th>
2017-09-01 10:53:42 +0700

    Disable enter detech function

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -170,13 +170,14 @@ function deleteUser(event) {
 
 };
 
-
-// get endter key press ===================================================
+/*
+// get enter key press ===================================================
 $(document).keypress(function (e) {
     if (e.which == 13) {
         alert('enter key is pressed');
     }
 });
+*/
 
 
 // export function for test function
