4eb00bbd33c46f471de639f64373314c43076a55
assetthawut <s.sukmaung@togetherteam.co.th>
2017-09-04 17:34:53 +0700

    sinon spy done

--- test/4sep.test.js
+++ test/4sep.test.js
@@ -1,5 +1,6 @@
 const sinon = require("sinon");
 const chai  = require("chai");
+var expect = chai.expect;
 
 
 class DB {
@@ -17,13 +18,46 @@ class DB {
     return this.db.get(id)
   }
 
+sum(){
+     var sumsum = 1+1;
+  }
+
+
   // print something 
   echo(word){
+      this.sum();
+      this.sum();
+      this.sum();
       return console.log(word);
   }
+
+ 
+
 }
 
 
 const joe = new DB("PowerRenger");
 
 joe.echo("Test");
+
+
+describe("Test Sinon Done!!!",function(){
+    it(" First Test Spy in function echo ",function(){
+        const spy  = sinon.spy(joe,'sum');
+        joe.echo("Hello");
+        expect(spy.calledOnce ).to.equal(true);
+    });
+
+    it(" Second Test Stub",function(){
+        // db = {};
+        // db.echo = sinon.stub();
+        // db.echo.withArgs('Test').return("Hi");
+        // const doc = new Document(sett);
+        // expect(doc.getData('Test')).to.deep.equal("Hi");
+
+
+
+
+    });
+
+});
